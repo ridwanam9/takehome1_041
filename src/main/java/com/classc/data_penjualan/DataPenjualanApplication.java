db749f473846d5216a61c476bbdacc10bdd2eee3
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2022-10-28 23:21:31 +0700

    first commit

---
+++ src/main/java/com/classc/data_penjualan/DataPenjualanApplication.java
@@ -0,0 +1,13 @@
+package com.classc.data_penjualan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPenjualanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPenjualanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/classc/data_penjualan/DataPenjualanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classc.data_penjualan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPenjualanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
